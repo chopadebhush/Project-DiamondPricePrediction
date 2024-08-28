02e1df12e12db34ab56218281350ccc59a22e5b1
Bhushan <chopadebhush@gmail.com>
2024-08-28 10:12:09 +0530

    data ingestion update

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,12 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
+
+def error_message_detail(error, error_detail: sys):
+    """ 
+    exc_info==>>returns information about the most recently handled exception in the current thread Return current exception information: (type->exc_type, value->exc_value, traceback->exc_tb)
+    """
+    _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
 
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
@@ -11,21 +15,23 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
+
 class CustomException(Exception):
-    
-    def __init__(self, error_message, error_detail:sys):
+
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail)
 
     def __str__(self):
-        return self.error_message  
-    
+        return self.error_message
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     logging.info("Logging has started")
 
     try:
-        a=1/0
+        a = 1/0
     except Exception as e:
-        logging.info('Division by zero') 
-        raise CustomException(e,sys)
+        logging.info('Division by zero')
+        raise CustomException(e, sys)
