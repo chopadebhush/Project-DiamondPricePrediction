3de4bb44767da842872c7efb092b3ebadd68e4ca
Bhushan <chopadebhush@gmail.com>
2024-09-11 11:00:06 +0530

    Data_transformation_complete

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -5,9 +5,16 @@ from src.exception import CustomException
 
 import pandas as pd
 from src.componants.data_ingestion import DataIngestion
+from src.componants.data_transformation import DataTransformation
 
 
 if __name__ == "__main__":
+    # Data ingestion object
     obj = DataIngestion()
     train_data_path, test_data_path = obj.initiate_data_ingestion()
     print(train_data_path, test_data_path)
+
+    # Data transformation object
+    data_transformation = DataTransformation()
+    train_arr, test_arr, obj_path = data_transformation.initiate_data_transformation(
+        train_data_path, test_data_path)
